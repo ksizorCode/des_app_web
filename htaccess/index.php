e8dc9d473fb8b5bf09778c6f0cbbcab7dc1f36a5
Miguel <miguelestebn@gmail.com>
2023-05-23 15:42:00 +0200

    a

---
+++ htaccess/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+ 
+<header>
+    <nav>
+        <ul>
+            <li><a href="./inicio">Inicio</a></li>
+            <li><a href="./login">Iniciar Sesión</a></li>
+            <li><a href="./registro">Registro</a></li>
+        </ul>
+    </nav>
+
+</header>
+<main><h1>tas index.php</h1></main>
+<footer></footer>
+
+    <h1>htacces</h1>
+    <p>htaccess es un archivo que se ejecuta en servidore con Apache y nos permite realizar ciertas accioens como restrigir el acceso a una directorio, forzar la carga de https, quitar o poner www, limpiar la extensión de las direcciones (por ejemplo quitar .html o .php). O incluso redirigir a apartados o directorios que no existen sustituyéndolos por otros a los que el usuario no puede ver su ruta.</p>
+    
+</body>
+</html>
