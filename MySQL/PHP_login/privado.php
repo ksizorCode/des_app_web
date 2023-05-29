0fd81d13fe3b7d313918dd933efcbe8f7aac44dd
Miguel <miguelestebn@gmail.com>
2023-05-29 17:47:07 +0200

    Update privado.php

--- MySQL/PHP_login/privado.php
+++ MySQL/PHP_login/privado.php
@@ -6,14 +6,13 @@
 session_start();
 
 // Comprobamos si existe con isset()
-if (isset($_SESSION['nombre']))  {
+if (isset($_SESSION['nombre'])) {
     // Si esta identificado, en otras palabras existe la variable, le saludamos
-    
     echo 'Hola ' . $_SESSION['nombre'];
     echo '<p>Texto super secreto y privado.</p>';
     echo '<img src="tesoro.jpg">';
     echo '<h2>¿No te gusta votar? pues toma dos tazas</h2>';
-
+    
 } else {
     // En caso contrario redirigimos el visitante a otra página
     header('Location: expulsado.php');
