de3d72ee252fe59781b0654275564aae8803831a
Miguel <miguelestebn@gmail.com>
2023-06-15 16:30:26 +0200

    aa

--- MVC/themes/augustheme/header.php
+++ MVC/themes/augustheme/header.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><? apartadoyTitulo(); ?></title>
         <link rel="stylesheet" href="<? ruta_a_theme()?>/style.css">
+        <title><? apartadoyTitulo(); ?></title>
         <?php finHead();?>
     </head>
     <body>
